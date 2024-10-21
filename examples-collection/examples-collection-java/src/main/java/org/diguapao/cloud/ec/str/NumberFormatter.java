70ab909e55d9be5cb7076b38b6ef3acfc08d1cd1
lipiao <changgelp@163.com>
2024-10-21 14:15:18 +0800

    字符串处理-数字格式化

--- examples-collection/examples-collection-java/src/main/java/org/diguapao/cloud/ec/str/NumberFormatter.java
+++ examples-collection/examples-collection-java/src/main/java/org/diguapao/cloud/ec/str/NumberFormatter.java
@@ -17,6 +17,11 @@ public class NumberFormatter {
         System.out.println(foo(12345, 4));
     }
 
+    // 把整数 v 转换成 String，但从个位起每隔 x(x>0) 个数字就加一个逗号(,)分隔。例如：
+    // foo(12, 2) => "12"
+    // foo(1234, 3) => "1,234"
+    // foo(123456, 2) => "12,34,56"
+    // foo(12345, 4) => "1,2345"
     public static String foo(int v, int x) {
         if (x <= 0) {
             throw new RuntimeException("X必须大于0");
@@ -31,12 +36,22 @@ public class NumberFormatter {
             return str;
         }
 
+        // 方法一
         // 使用 StringBuilder 来构建结果字符串
-        StringBuilder result = new StringBuilder(str);
+//        StringBuilder result = new StringBuilder(str);
+//        // 从右到左每隔 x 个字符插入一个逗号
+//        for (int i = len - x; i > 0; i -= x) {
+//            result.insert(i, ',');
+//        }
 
-        // 从右到左每隔 x 个字符插入一个逗号
-        for (int i = len - x; i > 0; i -= x) {
-            result.insert(i, ',');
+        //方法二
+        char[] arr = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (int i = arr.length - 1; i > 0; i++) {
+            if ((i) % x == 0 && i < arr.length - 1 && i != 0) {
+                result.append(",");
+            }
+            result.append(arr[i]);
         }
 
         return result.toString();
