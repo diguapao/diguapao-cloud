7084b6784125ba006ac756dd6179239727c049b7
DiGuaPao <changgelp@163.com>
2025-02-13 15:27:09 +0800

    引入加密组件

--- examples-collection/examples-java/examples-jasypt/src/main/java/org/diguapao/cloud/framework/jasypt/JasyptApplication.java
+++ examples-collection/examples-java/examples-jasypt/src/main/java/org/diguapao/cloud/framework/jasypt/JasyptApplication.java
@@ -1,6 +1,5 @@
 package org.diguapao.cloud.framework.jasypt;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
