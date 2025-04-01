cf325820cb9367bd70dfb6474e54a3284e538d24
DiGuaPao <changgelp@163.com>
2025-04-01 13:48:53 +0800

    创建 spring ai 项目

---
+++ examples-collection/examples-java/examples-ai/spring-ai/src/test/java/org/diguapao/cloud/framework/spring/ai/spring_ai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diguapao.cloud.framework.spring.ai.spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
