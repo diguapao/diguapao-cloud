baf08af222728c1db2e921e2d8741885fea8baf8
DiGuaPao <changgelp@163.com>
2025-07-04 17:07:20 +0800

    -

--- non-echnology-doc/Jinan-University/000-C_Programming/C5_source_program(17.9)/Chapter3/c3-7.c
+++ non-echnology-doc/Jinan-University/000-C_Programming/C5_source_program(17.9)/Chapter3/c3-7.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main()
-{float a;
- a=10000/3.0;
- printf("%f\n",a);
- return 0;
+{
+    float a;
+    a = 10000 / 3.0;
+    printf("%f\n", a); // 默认保留小数点后6位
+    return 0;
 }
-
-
-
--- non-echnology-doc/Jinan-University/000-C_Programming/C5_source_program(17.9)/Chapter3/c3-9-2.c
+++ non-echnology-doc/Jinan-University/000-C_Programming/C5_source_program(17.9)/Chapter3/c3-9-2.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-int main ( )                
-{ putchar(getchar());           // 将接收到的字符输出 
-  putchar(getchar());           // 将接收到的字符输出 
-  putchar(getchar());           // 将接收到的字符输出 
-  putchar('\n'); 
+int main()
+{
+  putchar(getchar()); // 将接收到的字符输出
+  putchar(getchar()); // 将接收到的字符输出
+  putchar(getchar()); // 将接收到的字符输出
+  putchar('\n');
   return 0;
 }
-
-
-
-
